14bc5d0e25112034b7e2ee74ab7eb86a5c7d1bd7
vboxuser <vboxuser@cs105ubuntu.myguest.virtualbox.org>
2023-03-30 16:51:57 -0700

    Changes to be committed: 	modified:   cs205_final_exam.sh 	new file:   screenshots/Screenshot from 2023-03-30 16-49-19.png

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,20 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+
+FILE="$1"
+#here, our file is our first variable, in this case being pokemon.dat
+TOTAL=$(awk 'END {print NR-1}' $FILE)
+#NR-1 ensures that the first row is not counted when displaying the total amount of pokemon
+HP=$(awk 'BEGIN{FS "\t"}NR-1{sum+=$6}END{print sum/(NR-1)}' $FILE)
+#Here, the field separator is identified as tab. the total of column 6 is added then divided by the total number of inputs
+ATTACK=$(awk 'BEGIN{FS "\t"}NR-1{sum+=$7}END{print sum/(NR-1)}' $FILE)
+#this is the same as the other average, with the 6th column being replaced by the 7th
+
+echo "===== SUMMARY OF DATA FILE ====="
+echo "	File name: $FILE"
+echo "	Total Pokemon: ${total}"
+echo "	Avg. HP: ${HP}"
+echo "	Avg. Attack: ${ATTACK}"
